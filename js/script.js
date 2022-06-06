0c704b2b18f1b9f93f4851a536dc53f71cee6d8b
zzwander <zuri.zain1@gmail.com>
2022-06-06 19:51:04 +0100

    files uploaded

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function togglePopupHead(){
+    document.getElementById("popup-1").classList.toggle("active")
+}
+function togglePopupPaw(){
+    document.getElementById("popup-2").classList.toggle("active")
+}
+function togglePopupBack(){
+    document.getElementById("popup-3").classList.toggle("active")
+}
+function togglePopupTail(){
+    document.getElementById("popup-4").classList.toggle("active")
+}
+function togglePopupBelly(){
+    document.getElementById("popup-5").classList.toggle("active")
+}
+
+function togglePopupCatEars(){
+    document.getElementById("popup-6").classList.toggle("active")
+}
+function togglePopupCatEyes(){
+    document.getElementById("popup-7").classList.toggle("active")
+}
+function togglePopupCatFur(){
+    document.getElementById("popup-8").classList.toggle("active")
+}
+function togglePopupCatTail(){
+    document.getElementById("popup-9").classList.toggle("active")
+}
+function togglePopupCatPaw(){
+    document.getElementById("popup-10").classList.toggle("active")
+}
+
+
---
+++ js/sign-up.js
@@ -0,0 +1,7 @@
+const init = function(){
+    document.getElementById('submitButton').addEventListener('click', send);
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+
